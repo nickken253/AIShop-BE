a76dec1d6465fcfcb8ee269a9397ce0076803bb3
NickKen <hoanganh.nickken@gmail.com>
2024-02-22 05:29:26 +0700

    [1.0.0] Sửa lại biến env và đường dẫn ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "AIShop",
-      script: "./src/server.js",
+      script: "src/server.js",
       watch: true,
       env: {
         NODE_ENV: "production",
