cb1bd0d3605d06da2f741b2727821a51ac6a80cd
NickKen <hoanganh.nickken@gmail.com>
2024-02-23 12:13:25 +0700

    [1.0.0] Update Cart Controller

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -51,181 +51,168 @@ export const deleteCart = async (req, res) => {
     }
     await cart.remove();
     res.json({ message: "Cart deleted" });
-    }
-    catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error deleting cart" });
-    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting cart" });
+  }
 };
 
 export const addCartItem = async (req, res) => {
-    const userId = req.params.userId;
-    try {
-        const cart = await Cart.findOne({ userId: userId });
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        // Kiểm tra nếu sản phẩm đã tồn tại thì cộng dồn lên
-        let item = cart.items.find((item) => item.productId === req.body.productId);
-        if (item) {
-            item.quantity += req.body.quantity;
-            item.totalPrice += req.body.totalPrice;
-            cart.totalQuantity += req.body.quantity;
-            cart.totalPrice += req.body.totalPrice;
-            await cart.save();
-            res.json(cart);
-            return;
-        }
-        // Nếu sản phẩm chưa tồn tại thì thêm mới vào giỏ hàng
-        item = req.body;
-        cart.items.push(item);
-        cart.totalQuantity += item.quantity;
-        cart.totalPrice += item.totalPrice;
-        await cart.save();
-        res.json(cart);
+  const userId = req.params.userId;
+  try {
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error adding item to cart" });
+    let itemIndex = cart.items.findIndex(
+      (item) => item.productId === req.body.productId
+    );
+    if (itemIndex !== -1) {
+      // Nếu sản phẩm đã tồn tại, cập nhật thông tin của nó
+      cart.items[itemIndex].quantity += req.body.quantity;
+      cart.items[itemIndex].totalPrice += req.body.totalPrice;
+      cart.totalQuantity += req.body.quantity;
+      cart.totalPrice += req.body.totalPrice;
+    } else {
+      // Nếu sản phẩm chưa tồn tại, thêm mới vào giỏ hàng
+      cart.items.push(req.body);
+      cart.totalQuantity += req.body.quantity;
+      cart.totalPrice += req.body.totalPrice;
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error adding item to cart" });
+  }
 };
 
 export const updateCartItem = async (req, res) => {
-    const userId = req.params.userId;
-    const productId = req.params.productId;
-    try {
-        const cart = await Cart.findOne({ userId: userId });
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        const item = cart.items.id(productId);
-        if (!item) {
-            return res.status(404).json({ message: "Item not found" });
-        }
-        item.quantity = req.body.quantity;
-        item.totalPrice = req.body.totalPrice;
-        cart.totalQuantity = 0;
-        cart.totalPrice = 0;
-        for (const item of cart.items) {
-            cart.totalQuantity += item.quantity;
-            cart.totalPrice += item.totalPrice;
-        }
-        await cart.save();
-        res.json(cart);
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  try {
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error updating item in cart" });
+    const item = cart.items.id(productId);
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
     }
+    item.quantity = req.body.quantity;
+    item.totalPrice = req.body.totalPrice;
+    cart.totalQuantity = 0;
+    cart.totalPrice = 0;
+    for (const item of cart.items) {
+      cart.totalQuantity += item.quantity;
+      cart.totalPrice += item.totalPrice;
+    }
+    await cart.save();
+    res.json(cart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating item in cart" });
+  }
 };
 
 export const deleteCartItem = async (req, res) => {
-    const userId = req.params.userId;
-    const productId = req.params.productId;
-    try {
-        const cart = await Cart.findOne({ userId: userId });
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        const item = cart.items.id(productId);
-        if (!item) {
-            return res.status(404).json({ message: "Item not found" });
-        }
-        cart.totalQuantity -= item.quantity;
-        cart.totalPrice -= item.totalPrice;
-        item.remove();
-        await cart.save();
-        res.json(cart);
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  try {
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error deleting item from cart" });
+    const item = cart.items.id(productId);
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
     }
+    cart.totalQuantity -= item.quantity;
+    cart.totalPrice -= item.totalPrice;
+    item.remove();
+    await cart.save();
+    res.json(cart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting item from cart" });
+  }
 };
 
 export const emptyCart = async (req, res) => {
-    const userId = req.params.userId;
-    try {
-        const cart = await Cart.findOne({ userId: userId });
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        cart.items = [];
-        cart.totalQuantity = 0;
-        cart.totalPrice = 0;
-        await cart.save();
-        res.json(cart);
+  const userId = req.params.userId;
+  try {
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error emptying cart" });
-    }
+    cart.items = [];
+    cart.totalQuantity = 0;
+    cart.totalPrice = 0;
+    await cart.save();
+    res.json(cart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error emptying cart" });
+  }
 };
 
 export const getCartItem = async (req, res) => {
-    const userId = req.params.userId;
-    const productId = req.params.productId;
-    try {
-        const cart = await Cart.findOne({ userId: userId });
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        const item = cart.items.id(productId);
-        if (!item) {
-            return res.status(404).json({ message: "Item not found" });
-        }
-        res.json(item);
+  const userId = req.params.userId;
+  const productId = req.params.productId;
+  try {
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error fetching item from cart" });
+    const item = cart.items.id(productId);
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
     }
+    res.json(item);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching item from cart" });
+  }
 };
 
 export const getCartItems = async (req, res) => {
-    const userId = req.params.userId;
-    try {
-        const cart = await Cart.findOne({ userId: userId });
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        res.json(cart.items);
+  const userId = req.params.userId;
+  try {
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error fetching items from cart" });
-    }
+    res.json(cart.items);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching items from cart" });
+  }
 };
 
 export const getCartTotalQuantity = async (req, res) => {
-    const userId = req.params.userId;
-    try {
-        const cart = await Cart.findOne({ userId: userId });
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        res.json(cart.totalQuantity);
+  const userId = req.params.userId;
+  try {
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error fetching total quantity from cart" });
-    }
+    res.json(cart.totalQuantity);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Error fetching total quantity from cart" });
+  }
 };
 
 export const getCartTotalPrice = async (req, res) => {
-    const userId = req.params.userId;
-    try {
-        const cart = await Cart.findOne({ userId: userId });
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        res.json(cart.totalPrice);
+  const userId = req.params.userId;
+  try {
+    const cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error fetching total price from cart" });
-    }
+    res.json(cart.totalPrice);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching total price from cart" });
+  }
 };
-
-
-
