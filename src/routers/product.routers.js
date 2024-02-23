e5379c4bc8bf14627d80b5ea7bd9d2b86d0cb484
NickKen <hoanganh.nickken@gmail.com>
2024-02-23 11:22:35 +0700

    [1.0.0] Add New Product Routers

--- src/routers/product.routers.js
+++ src/routers/product.routers.js
@@ -19,8 +19,9 @@ router.get("/get-sub-category/:master", listSubCategoryByMaster);
 router.get("/get-product-by-page", getProductByPage);
 router.get("/get-list-product-image-price-name", getListProductImagePriceName);
 router.get("/get-all-product", getAllProduct);
+router.get("/get-product-by-page/:master", getProductByPagemasterCategory);
 router.get("/get-product-by-page-master-category/:master", getProductByPagemasterCategory);
-router.get("/get-product-by-page-master-category-sub-category/:master/:sub", getProductByPagemasterCategorysubCategory);
+// router.get("/get-product-by-page-master-category-sub-category/:master/:sub", getProductByPagemasterCategorysubCategory);
 router.get("/get-product-by-page-master-category/:master/:sub", getProductByPagemasterCategorysubCategory);
 
 export default router;
